14899d7bb4b6bd6bd74b3a6461e9f893ace7ec99
xtrm-en <26600206+xtrm-en@users.noreply.github.com>
2022-07-06 02:23:04 +0200

    gradle: updated dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,14 @@
 plugins {
-    kotlin("jvm") version "1.4.21"
+    kotlin("jvm") version "1.7.0"
     `java-library`
 }
 
 repositories {
-    jcenter()
-    maven(url = "https://libraries.minecraft.net")
+    mavenCentral()
+    maven("https://libraries.minecraft.net")
 }
 
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
-    api("com.mojang:brigadier:1.0.17")
+    api("com.mojang", "brigadier", "1.0.18")
 }
